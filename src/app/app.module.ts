f62a14c5b6d4c14f8b4d8a55a2bd793dd7719181
uyle1234 <linhconuong2k3@gmail.com>
2022-01-14 12:33:25 +0700

    caculato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaytinhComponent } from './maytinh/maytinh.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MaytinhComponent
   ],
   imports: [
     BrowserModule,
